eea47c3e9f1d15e2fb91159e70fc44b08cd59b0c
Railken <railken@protonmail.ch>
2020-09-26 22:02:17 +0200

    improve tests

--- tests/HasManyTest.php
+++ tests/HasManyTest.php
@@ -13,7 +13,7 @@ class HasManyTest extends BaseTest
     public function startingHasMany($column, $params = [])
     {
         Schema::table('foo', function (BluePrint $table) use ($column) {
-            if (Schema::hasColumn('foo', 'parent_id')) {
+            if (Schema::hasColumn('foo', $column)) {
                 $table->dropColumn($column);
             }
         });
@@ -32,7 +32,7 @@ class HasManyTest extends BaseTest
         ]);
     }
 
-    public function testHasManyWithTarget()
+    public function testHasMany()
     {
         $this->startingHasMany('foo_id', [
         ]);
@@ -49,10 +49,10 @@ class HasManyTest extends BaseTest
         );
     }
 
-    public function testHasManyWithTargetAndLocalKey()
+    public function testHasManyWithLocalKey()
     {
         $this->startingHasMany('foo_id', [
-             'local_key' => 'localKey',
+             'localKey' => 'localKey',
         ]);
 
         $parent = Foo::create(['name' => 'Parent']);
@@ -65,6 +65,41 @@ class HasManyTest extends BaseTest
         $this->assertEquals(
             "select * from `foo` where `foo`.`foo_id` = '2' and `foo`.`foo_id` is not null and `foo`.`deleted_at` is null",
             $this->getQuery($parent->children())
+        );
+    }
+
+    public function testHasManyWithFilter()
+    {
+        $this->startingHasMany('parent_id', [
+            'foreignKey' => 'parent_id'
+        ]);
+
+        RelationSchema::create([
+            'name'    => 'redChildren',
+            'type'    => 'HasMany',
+            'data'    => 'foo',
+            'payload' => Yaml::dump([
+                'target' => 'foo',
+                'foreignKey' => 'parent_id',
+                'filter' => "children.name eq 'Red' and children.name != 'Blue'"
+            ])
+        ]);
+
+        $parent = Foo::create(['name' => 'Parent']);
+        $redChild1 = Foo::create(['name' => 'Red 1']);
+        $redChild2 = Foo::create(['name' => 'Red 2']);
+        $blueChild = Foo::create(['name' => 'Blue']);
+
+        $parent->children()->save($redChild1);
+        $parent->children()->save($redChild2);
+        $parent->children()->save($blueChild);
+
+        $this->assertEquals($redChild1->name, $parent->children[0]->name);
+        $this->assertEquals($redChild2->name, $parent->children[1]->name);
+        $this->assertEquals($blueChild->name, $parent->children[2]->name);
+        $this->assertEquals(
+            "select `foo`.* from `foo` left join `foo` as `children` on `foo`.`id` = `children`.`parent_id` and `children`.`deleted_at` is null where (`children`.`name` = 'Red' and `children`.`name` != 'Blue') and `foo`.`deleted_at` is null",
+            $this->getQuery($parent->redChildren())
         );
     }
 }
