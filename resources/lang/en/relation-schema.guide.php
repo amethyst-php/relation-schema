dfc8e01d2ff6ca3843018f41e453b2a49de20696
Railken <railken@protonmail.ch>
2020-10-05 09:32:46 +0200

    lang

---
+++ resources/lang/en/relation-schema.guide.php
@@ -0,0 +1,5 @@
+<?php
+
+return <<<EOD
+# A simple Guide
+EOD;
--- resources/lang/en/relation-schema.php
+++ resources/lang/en/relation-schema.php
@@ -1,7 +1,28 @@
 <?php
 
 return [
-    'name' => 'RelationSchema',
-    'description' => 'RelationSchema',
-    'attributes' => []
+    'name' => 'Relation Schema',
+    'description' => 'Create your own custom relations between Models',,
+    'attributes' => [
+        'data' => [
+            'label' => 'Data',
+            'description' => 'Which data should the relation attached to'
+        ],
+        'name' => [
+            'label' => 'Name',
+            'description' => 'What is the name of the relation?'
+        ],
+        'description' => [
+            'label' => 'Description',
+            'description' => 'A brief description'
+        ],
+        'type' => [
+            'label' => 'Type',
+            'description' => 'Which type is this relation?'
+        ],
+        'payload' => [
+            'label' => 'Payload',
+            'description' => 'A set of instruction to better define the relation, this changes based on the type of the relation'
+        ]
+    ]
 ];
